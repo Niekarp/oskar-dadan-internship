a5cd88b3f7fc87cd6686e8a63f7450cddcf70288
Oskar Dadan <skrzynkanof@gmail.com>
2019-09-24 20:17:50 +0200

    refactor: add and apply user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  constructor(
+      public id: number,
+      public name: string,
+      public username: string,
+      public email?:	string,
+      public address?:	object,
+      public phone?:	string,
+      public website?:	string,
+      public company?:	object) { }
+}
--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-users-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-  public users = ['user1', 'user2', 'user3'];
+  public users: User[] = [
+    { id: 1, name: 'user1', username: 'usr1' },
+    { id: 2, name: 'user2', username: 'usr2' },
+    { id: 3, name: 'user3', username: 'usr3' },
+  ];
 
   constructor() { }
 
