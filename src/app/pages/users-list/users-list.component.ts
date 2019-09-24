6d29614365dde5ebe265d56c481cb74f19f1805c
Oskar Dadan <skrzynkanof@gmail.com>
2019-09-25 00:06:43 +0200

    feat(users-list): add search input filtering

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -8,13 +8,33 @@ import { ApiClientService } from 'src/app/services/api-client/api-client.service
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-  public users: User[];
+  public searchedUsers: User[];
+  private users: User[];
 
   constructor(private api: ApiClientService) {
-    this.api.getUsers().subscribe(users => this.users = users);
+    this.api.getUsers().subscribe(users => {
+      this.users = users;
+      this.updateSearchedUsers('');
+    });
   }
 
   ngOnInit() {
+  }
+
+  public onSearchInputKeydown(event: Event): void {
+    this.updateSearchedUsers((event.target as HTMLInputElement).value);
+  }
+
+  private updateSearchedUsers(searchText: string): void {
+    this.searchedUsers = /\S/.test(searchText) ?
+      this.users.filter(user => this.searchName(user.name, searchText)) :
+      this.users;
+  }
+
+  private searchName(name: string, searchText: string): boolean {
+    const searchPattern = new RegExp(searchText.toLowerCase().replace(/\s/g, ''));
+    const nameParts = name.toLowerCase().replace(/\s/g, '');
+    return searchPattern.test(nameParts);
   }
 
 }
