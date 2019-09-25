5f3995a9ec930911049c2936dade15f8e26bedc5
Oskar Dadan <skrzynkanof@gmail.com>
2019-09-25 02:59:02 +0200

    feat(api-client): add request retry and error handling

--- src/app/services/api-client/api-client.service.ts
+++ src/app/services/api-client/api-client.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, retryWhen, delay, tap, timeout, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,37 @@ export class ApiClientService {
 
   public getUsers(): Observable<User[]> {
     const url = `${this.apiUrl}/users`;
-    return this.http.get<User[]>(url);
+    const request = this.http.get<User[]>(url);
+    return this.pipeStandardRequestStrategy(request);
   }
+
+  private pipeStandardRequestStrategy<T>(observable: Observable<T>, stopApp = true): Observable<T> {
+    const retryCount = 3;
+    const timeoutTime = 6500;
+    const retryInterval = 5000;
+
+    return observable.pipe(
+        timeout(timeoutTime),
+        retryWhen(errors => {
+          return errors.pipe(
+              map((error, index) => {
+                if (index === retryCount) {
+                  throw error;
+                }
+                return error;
+              }),
+              delay(retryInterval),
+              take(retryCount + 1));
+        }),
+        catchError(err => this.handleError(err, stopApp)));
+  }
+
+  private handleError(error: Error, stopApp: boolean): Observable<never> {
+    console.error(error);
+    if (stopApp) {
+      alert('Cannot connect to the server. Please visit the website later');
+    }
+    return throwError('api request error occured');
+  }
+
 }
